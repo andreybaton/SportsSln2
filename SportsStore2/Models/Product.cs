2192e0a8528687a4b938e68621d69e1118bcc3a0
Andrew <Andrew@DESKTOP-AOBRPHQ>
2025-04-08 00:46:12 +0400

    Добавьте файлы проекта.

---
+++ SportsStore2/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace SportsStore2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Column(TypeName ="decimal(8, 2)")]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
